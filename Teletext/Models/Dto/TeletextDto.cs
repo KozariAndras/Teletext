cc4a1a0d60723fa49647a852ff4515c0e6573874
András Kozári <pordnb@INF.ELTE.HU>
2023-12-09 17:37:43 +0100

    Reworked DTOs

--- Teletext/Models/Dto/TeletextDto.cs
+++ Teletext/Models/Dto/TeletextDto.cs
@@ -2,23 +2,36 @@
 
 public class TeletextDto
 {
-    public TeletextUser User { get; set; }
-    public List<ChannelDto> Channels { get; set; }
+    public TeletextUser? User { get; set; }
+    public List<ChannelDto>? Channels { get; set; }
 }
 
 public class ChannelDto
 {
-    public List<ProgramDto> Programs { get; set; }
-    public string Name { get; set; }
+    public List<ProgramDto>? Programs { get; set; }
+
+    public long Id { get; set; }
+
+    public string? Name { get; set; }
+    public int Number { get; set; }
 
 }
 
 public class ProgramDto
 {
-    public List<AiringScheduleDto> AiringSchedules { get; set; }
-    public string Name { get; set; }
+    public List<AiringScheduleDto>? AiringSchedules { get; set; }
+    public long Id { get; set; }
+    public string? Name { get; set; }
+    public int Duration { get; set; }
+    public int AgeRating { get; set; }
     public bool IsFavourite { get; set; }
 }
 
 public class AiringScheduleDto
-{ }
+{
+    public long Id { get; set; }
+    public DateOnly StartDate { get; set; }
+    public DayOfWeek Day { get; set; }
+    public TimeSpan Time { get; set; }
+
+}
